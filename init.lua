618cc15e9a22570e4a32e558cd1c6899f186dc4f
Elie Guedj <elie-externe.guedj@datanumia.com>
2024-08-23 13:02:16 +0200

    Add plugins

--- init.lua
+++ init.lua
@@ -151,6 +151,9 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
+-- Save file with sudo with w!!
+vim.keymap.set('c', 'w!!', ':w ! sudo tee % > /dev/null')
+
 -- Show which line your cursor is on
 vim.opt.cursorline = true
 
@@ -198,6 +201,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.filetype.add {
+  pattern = { ['.*/hypr/.*%.conf'] = 'hyprlang' },
+}
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
@@ -238,6 +245,12 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  {
+    'NvChad/nvim-colorizer.lua', -- Colorize hexadecimal colors
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
@@ -250,6 +263,8 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
+
+  'mbbill/undotree',
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
@@ -267,6 +282,41 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
+  },
+
+  {
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+      'stevearc/dressing.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+    cmd = 'Nerdy',
+  },
+
+  -- lazy.nvim
+  {
+    'robitx/gp.nvim',
+    config = function()
+      local conf = {
+        -- For customization, refer to Install > Configuration in the Documentation/Readme
+        openai_api_key = { 'cat', '/home/eguedj/.config/nvim/.openai-api-key' },
+      }
+      require('gp').setup(conf)
+
+      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+    end,
+  },
+
+  {
+    'stevearc/oil.nvim',
+    opts = {
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
@@ -299,7 +349,121 @@ require('lazy').setup({
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        {
+          mode = { 'v' },
+          nowait = true,
+          remap = false,
+          { '<C-g><C-t>', ":<C-u>'<,'>GpChatNew tabnew<cr>", desc = 'ChatNew tabnew' },
+          { '<C-g><C-v>', ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = 'ChatNew vsplit' },
+          { '<C-g><C-x>', ":<C-u>'<,'>GpChatNew split<cr>", desc = 'ChatNew split' },
+          { '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", desc = 'Visual Append (after)' },
+          { '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", desc = 'Visual Prepend (before)' },
+          { '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", desc = 'Visual Chat New' },
+          { '<C-g>g', group = 'generate into new ..' },
+          { '<C-g>ge', ":<C-u>'<,'>GpEnew<cr>", desc = 'Visual GpEnew' },
+          { '<C-g>gn', ":<C-u>'<,'>GpNew<cr>", desc = 'Visual GpNew' },
+          { '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", desc = 'Visual Popup' },
+          { '<C-g>gt', ":<C-u>'<,'>GpTabnew<cr>", desc = 'Visual GpTabnew' },
+          { '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", desc = 'Visual GpVnew' },
+          { '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", desc = 'Implement selection' },
+          { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
+          { '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", desc = 'Visual Chat Paste' },
+          { '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", desc = 'Visual Rewrite' },
+          { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
+          { '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", desc = 'Visual Toggle Chat' },
+          { '<C-g>w', group = 'Whisper' },
+          { '<C-g>wa', ":<C-u>'<,'>GpWhisperAppend<cr>", desc = 'Whisper Append' },
+          { '<C-g>wb', ":<C-u>'<,'>GpWhisperPrepend<cr>", desc = 'Whisper Prepend' },
+          { '<C-g>we', ":<C-u>'<,'>GpWhisperEnew<cr>", desc = 'Whisper Enew' },
+          { '<C-g>wn', ":<C-u>'<,'>GpWhisperNew<cr>", desc = 'Whisper New' },
+          { '<C-g>wp', ":<C-u>'<,'>GpWhisperPopup<cr>", desc = 'Whisper Popup' },
+          { '<C-g>wr', ":<C-u>'<,'>GpWhisperRewrite<cr>", desc = 'Whisper Rewrite' },
+          { '<C-g>wt', ":<C-u>'<,'>GpWhisperTabnew<cr>", desc = 'Whisper Tabnew' },
+          { '<C-g>wv', ":<C-u>'<,'>GpWhisperVnew<cr>", desc = 'Whisper Vnew' },
+          { '<C-g>ww', ":<C-u>'<,'>GpWhisper<cr>", desc = 'Whisper' },
+          { '<C-g>x', ":<C-u>'<,'>GpContext<cr>", desc = 'Visual GpContext' },
+        },
+
+        -- NORMAL mode mappings
+        {
+          mode = { 'n' },
+          nowait = true,
+          remap = false,
+          { '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', desc = 'New Chat tabnew' },
+          { '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', desc = 'New Chat vsplit' },
+          { '<C-g><C-x>', '<cmd>GpChatNew split<cr>', desc = 'New Chat split' },
+          { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
+          { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
+          { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
+          { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
+          { '<C-g>g', group = 'generate into new ..' },
+          { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
+          { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
+          { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
+          { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
+          { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
+          { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
+          { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
+          { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
+          { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
+          { '<C-g>w', group = 'Whisper' },
+          { '<C-g>wa', '<cmd>GpWhisperAppend<cr>', desc = 'Whisper Append (after)' },
+          { '<C-g>wb', '<cmd>GpWhisperPrepend<cr>', desc = 'Whisper Prepend (before)' },
+          { '<C-g>we', '<cmd>GpWhisperEnew<cr>', desc = 'Whisper Enew' },
+          { '<C-g>wn', '<cmd>GpWhisperNew<cr>', desc = 'Whisper New' },
+          { '<C-g>wp', '<cmd>GpWhisperPopup<cr>', desc = 'Whisper Popup' },
+          { '<C-g>wr', '<cmd>GpWhisperRewrite<cr>', desc = 'Whisper Inline Rewrite' },
+          { '<C-g>wt', '<cmd>GpWhisperTabnew<cr>', desc = 'Whisper Tabnew' },
+          { '<C-g>wv', '<cmd>GpWhisperVnew<cr>', desc = 'Whisper Vnew' },
+          { '<C-g>ww', '<cmd>GpWhisper<cr>', desc = 'Whisper' },
+          { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
+        },
+
+        -- INSERT mode mappings
+        {
+          mode = { 'i' },
+          nowait = true,
+          remap = false,
+          { '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', desc = 'New Chat tabnew' },
+          { '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', desc = 'New Chat vsplit' },
+          { '<C-g><C-x>', '<cmd>GpChatNew split<cr>', desc = 'New Chat split' },
+          { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
+          { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
+          { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
+          { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
+          { '<C-g>g', group = 'generate into new ..' },
+          { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
+          { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
+          { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
+          { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
+          { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
+          { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
+          { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
+          { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
+          { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
+          { '<C-g>w', group = 'Whisper' },
+          { '<C-g>wa', '<cmd>GpWhisperAppend<cr>', desc = 'Whisper Append (after)' },
+          { '<C-g>wb', '<cmd>GpWhisperPrepend<cr>', desc = 'Whisper Prepend (before)' },
+          { '<C-g>we', '<cmd>GpWhisperEnew<cr>', desc = 'Whisper Enew' },
+          { '<C-g>wn', '<cmd>GpWhisperNew<cr>', desc = 'Whisper New' },
+          { '<C-g>wp', '<cmd>GpWhisperPopup<cr>', desc = 'Whisper Popup' },
+          { '<C-g>wr', '<cmd>GpWhisperRewrite<cr>', desc = 'Whisper Inline Rewrite' },
+          { '<C-g>wt', '<cmd>GpWhisperTabnew<cr>', desc = 'Whisper Tabnew' },
+          { '<C-g>wv', '<cmd>GpWhisperVnew<cr>', desc = 'Whisper Vnew' },
+          { '<C-g>ww', '<cmd>GpWhisper<cr>', desc = 'Whisper' },
+          { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
+        },
       }
+    end,
+  },
+
+  -- Markdown preview in browser
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
     end,
   },
 
@@ -572,6 +736,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        ansiblels = {},
+        docker_compose_language_service = {},
+        bashls = {},
+        dockerls = {},
+        beautysh = {},
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
@@ -791,6 +960,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+
       vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
@@ -842,7 +1012,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'json', 'jsonc', 'hyprlang' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -881,10 +1051,10 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
